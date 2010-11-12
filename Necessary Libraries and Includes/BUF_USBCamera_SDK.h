e3b4c06bf031b597df8fd12a4a6335a37ce62563
Marc Gershow <gershow@gmail.com>
2010-11-12 18:06:54 -0500

    moved new mightex sdk header into here to avoid duplication

--- Necessary Libraries and Includes/BUF_USBCamera_SDK.h
+++ Necessary Libraries and Includes/BUF_USBCamera_SDK.h
@@ -1,7 +1,7 @@
-#ifndef _buf_usb_camera_sdk_h
-#define  _buf_usb_camera_sdk_h
+#ifndef _mightex_buf_usb_camera_sdk_h
+#define _mightex_buf_usb_camera_sdk_h
 
-#include <windows.h>
+#include<windows.h>
 
 // The following ifdef block is the standard way of creating macros which make exporting 
 // from a DLL simpler. All files within this DLL are compiled with the MTUSBDLL_EXPORTS
@@ -17,17 +17,15 @@ typedef unsigned int DEV_HANDLE;
 #define SDK_HANDLE_API extern "C" __declspec(dllexport) DEV_HANDLE _cdecl
 #define SDK_POINTER_API extern "C" __declspec(dllexport) unsigned char * _cdecl
 #else
-#define SDK_API __declspec(dllimport) SDK_RETURN_CODE _cdecl
-#define SDK_HANDLE_API __declspec(dllimport) DEV_HANDLE _cdecl
-#define SDK_POINTER_API  __declspec(dllimport) unsigned char * _cdecl
+#define SDK_API extern "C" __declspec(dllimport) SDK_RETURN_CODE _cdecl
+#define SDK_HANDLE_API extern "C" __declspec(dllimport) DEV_HANDLE _cdecl
+#define SDK_POINTER_API extern "C" __declspec(dllimport) unsigned char * _cdecl
 #endif
 
 #define GRAB_FRAME_FOREVER	0x8888
 
 #pragma pack(1)
-#ifdef __cplusplus
- extern "C" {
- #endif
+
 typedef struct {
     int Revision;
     // For Image Capture
@@ -77,6 +75,7 @@ SDK_API BUFUSB_StartFrameGrab( int TotalFrames );
 SDK_API BUFUSB_StopFrameGrab( void );
 SDK_API BUFUSB_ShowFactoryControlPanel( int DeviceID, char *passWord );
 SDK_API BUFUSB_HideFactoryControlPanel( void );
+SDK_API BUFUSB_SetBayerFilterType( int FilterType );
 SDK_API BUFUSB_SetResolution( int deviceID, int Resolution, int Bin, int BufferCnt );
 SDK_API BUFUSB_SetCustomizedResolution( int deviceID, int RowSize, int ColSize, int Bin, int BufferCnt );
 SDK_API BUFUSB_SetExposureTime( int DeviceID, int exposureTime );
@@ -87,13 +86,12 @@ SDK_API BUFUSB_SetGamma( int Gamma, int Contrast, int Bright, int Sharp );
 SDK_API BUFUSB_SetBWMode( int BWMode, int H_Mirror, int V_Flip );
 SDK_API BUFUSB_InstallFrameHooker( int FrameType, FrameDataCallBack FrameHooker );
 SDK_API BUFUSB_InstallUSBDeviceHooker( DeviceFaultCallBack USBDeviceHooker );
-//SDK_POINTER_API BUFUSB_GetCurrentFrame( int FrameType, int deviceID, byte* &FramePtr );
+SDK_POINTER_API BUFUSB_GetCurrentFrame( int FrameType, int deviceID, byte* &FramePtr );
 SDK_API BUFUSB_GetDevicesErrorState();
 SDK_API BUFUSB_SetGPIOConifg( int DeviceID, unsigned char ConfigByte );
 SDK_API BUFUSB_SetGPIOInOut( int DeviceID, unsigned char OutputByte,
                              unsigned char *InputBytePtr );
 
-#ifdef __cplusplus
- }
- #endif
+
 #endif
+
